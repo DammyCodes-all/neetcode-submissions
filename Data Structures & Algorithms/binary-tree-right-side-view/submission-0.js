5907396886545b6d703e9c0e82452bb01f756b37
Olagunju Alameen Olasubomi <harliarmeen@gmail.com>
2026-05-24 05:53:17 +0100

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        const res = []
+        if(!root) return res
+
+        let queue = []
+        queue.push(root)
+
+        while(queue.length){
+            const levelSize = queue.length
+            for(let i = 0; i < levelSize; i++){
+                const node =  queue.shift()
+                if(i === levelSize - 1) res.push(node.val)
+
+                if(node.left) queue.push(node.left)
+                if(node.right) queue.push(node.right)
+            }
+        }
+        return res
+    }
+}
