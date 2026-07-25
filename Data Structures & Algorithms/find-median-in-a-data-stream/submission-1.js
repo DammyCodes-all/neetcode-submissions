19b0ca9294f038017695e13eb2f66ab918f05867
Olagunju Alameen Olasubomi <harliarmeen@gmail.com>
2026-07-25 19:04:32 +0100

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.js
@@ -0,0 +1,41 @@
+class MedianFinder {
+    large = null
+    small = null
+
+    constructor() {
+        this.large = new MinPriorityQueue()
+        this.small = new MaxPriorityQueue()
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    addNum(num) {
+        if(this.small.isEmpty() || num < this.small.front()){
+            this.small.enqueue(num)
+        } else{
+            this.large.enqueue(num)
+        }
+
+        if(this.small.size() > this.large.size() + 1){
+            this.large.enqueue(this.small.dequeue())
+        } else if (this.large.size() > this.small.size() + 1) {
+            this.small.enqueue(this.large.dequeue());
+        }
+    }  
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if(this.small.size() > this.large.size()){
+            return this.small.front()
+        } else if(this.large.size() > this.small.size()){
+            return this.large.front()
+        } else {
+            return (this.large.front() + this.small.front()) / 2
+        }
+    }
+}
